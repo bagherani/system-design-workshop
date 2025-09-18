948863df2ad2fe9cc0f6b1dcc94d53638000b0f1
Mohi Bagherani <mohammad.bagherani@gmail.com>
2025-09-19 00:29:16 +0200

    feat: update dependencies and enhance users-service with IP address endpoint

--- apps/users-service-rest/src/main.ts
+++ apps/users-service-rest/src/main.ts
@@ -7,12 +7,19 @@ import express from 'express';
 import * as path from 'path';
 import { faker } from '@faker-js/faker';
 import { type User } from '@io/data-models';
+import { ip } from 'address';
 // TODO: complete the rest API
 // import { getOrders } from '@io/data-models';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({
+    message: `Server IP address: ${ip()}, Server port: ${process.env.PORT}`,
+  });
+});
 
 app.get('/api/users', (req, res) => {
   const users: User[] = Array.from({ length: 10 }, (_, i) => ({
